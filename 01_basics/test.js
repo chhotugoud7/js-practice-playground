7e09aa1b628eef6190bd3ac98c3c0bcdd39f00ea
chhotugoud7 <chhotu957588@gmail.com>
2026-05-02 22:38:47 +0530

    added js basics notes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("hello ji ")
+console.log("hello ji ");
+
+
+
+console.log("hello ji " + "how are you?");
+//checking branching 
+if (true) {
+    console.log("Branching is working!");
+};

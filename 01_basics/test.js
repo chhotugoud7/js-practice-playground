652642a6ff2610a6c6378c0bd159206d78848720
chhotugoud7 <chhotu957588@gmail.com>
2026-05-02 22:47:38 +0530

    checking branching

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -7,3 +7,10 @@ console.log("hello ji " + "how are you?");
 if (true) {
     console.log("Branching is working!");
 };
+
+//checking branching
+if (false) {
+    console.log("Branching is working!");
+} else {
+    console.log("Branching is working in else block!");
+}

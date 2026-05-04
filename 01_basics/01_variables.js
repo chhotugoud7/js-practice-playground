427688c0366e6775e5a80ee4375a1cca9a518897
chhotugoud7 <chhotu957588@gmail.com>
2026-05-04 16:05:06 +0530

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+
+
+
+const accountId = 123456;
+let accountEmail = "@gmail.com";
+var accountPassword = "password";
+accountCity = "New York";
+let  accountState;
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
